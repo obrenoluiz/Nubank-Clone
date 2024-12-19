fbc3fe6b0d50591a103e88743a36a2cfd4362332
brenoluizdev <brenoluizpv3@gmail.com>
2024-12-18 22:55:57 -0300

    initial colors and components

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,11 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:nubank_clone/utils/colors_theme.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-    );
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+        body: Container(
+      color: ColorsTheme.primaryPurple,
+      height: size.height * 0.35,
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(top: 50, left: 20),
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                    color: ColorsTheme.secondaryPurple,
+                    borderRadius: BorderRadius.circular(50)),
+                    child: const Center(
+                      child: Icon(
+                        Icons.person,
+                        color: Colors.white,
+                      )
+                    )
+              )
+            ],
+          )
+        ],
+      ),
+    ));
   }
 }
---
+++ lib/utils/colors_theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class ColorsTheme {
+  static const primaryPurple = Color.fromARGB(255, 108, 49, 211);
+  static const secondaryPurple = Color.fromARGB(255, 150, 78, 255);
+}
