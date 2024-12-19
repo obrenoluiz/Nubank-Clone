dfbb6c7e26db7137951231ce022096e334bcbb9b
brenoluizdev <brenoluizpv3@gmail.com>
2024-12-19 13:13:56 -0300

    Header App

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:nubank_clone/utils/colors_theme.dart';
 
 class HomePage extends StatelessWidget {
@@ -7,32 +8,111 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
+
     return Scaffold(
-        body: Container(
-      color: ColorsTheme.primaryPurple,
-      height: size.height * 0.35,
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Container(
-                margin: const EdgeInsets.only(top: 50, left: 20),
-                width: 50,
-                height: 50,
-                decoration: BoxDecoration(
-                    color: ColorsTheme.secondaryPurple,
-                    borderRadius: BorderRadius.circular(50)),
-                    child: const Center(
-                      child: Icon(
-                        Icons.person,
-                        color: Colors.white,
-                      )
-                    )
-              )
-            ],
-          )
-        ],
+      body: Container(
+        decoration: BoxDecoration(
+          color: ColorsTheme.primaryPurple,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              offset: const Offset(0, 5),
+              blurRadius: 10,
+            ),
+          ],
+        ),
+        height: size.height * 0.35,
+        child: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Colors.transparent,
+                    Colors.black.withOpacity(0.2),
+                  ],
+                  stops: const [0.8, 1.0],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: LayoutBuilder(
+                builder: (context, constraints) {
+                  double iconSize = constraints.maxWidth * 0.07;
+
+                  return Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 15.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Container(
+                              width: 50,
+                              height: 50,
+                              decoration: BoxDecoration(
+                                color: ColorsTheme.secondaryPurple,
+                                borderRadius: BorderRadius.circular(50),
+                              ),
+                              child: const Center(
+                                child: Icon(
+                                  Icons.person_outline_outlined,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                            Wrap(
+                              spacing: 20,
+                              children: [
+                                Icon(
+                                  Icons.visibility_outlined,
+                                  color: Colors.white,
+                                  size: iconSize,
+                                ),
+                                Icon(
+                                  FontAwesomeIcons.circleQuestion,
+                                  color: Colors.white,
+                                  size: iconSize - 0.2,
+                                ),
+                                Icon(
+                                  Icons.email_outlined,
+                                  color: Colors.white,
+                                  size: iconSize,
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      const Spacer(),
+                      const SizedBox(
+                        height: 40,
+                        child: Row(
+                          children: [
+                            Text(
+                              'Olá, Breno',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w400,
+                                fontFamily: 'Poppins',
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
--- lib/utils/colors_theme.dart
+++ lib/utils/colors_theme.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
 class ColorsTheme {
-  static const primaryPurple = Color.fromARGB(255, 108, 49, 211);
-  static const secondaryPurple = Color.fromARGB(255, 150, 78, 255);
+  static const primaryPurple = Color(0xff8109d0);
+  static const secondaryPurple = Color.fromARGB(255, 146, 11, 230);
 }
